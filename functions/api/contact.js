3309a185ce055de6bd7192f7fa51fa71d89e17ba
Benjamin Howe <ben@beh.uk>
2024-05-29 21:25:46 +0100

    Bug fixes

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -1,56 +1,51 @@
 export async function onRequest(context) {
-    if (context.request.method !== "POST") {
-        return new Response("Invalid request method.", { status: 405 });
-    }
+  if (context.request.method !== "POST") {
+    return new Response("Invalid request method.", { status: 405 });
+  }
 
-    // Get the form data.
-    const formData = await context.request.formData();
-    const body = Object.fromEntries(formData.entries());
+  const formData = await context.request.formData();
+  const body = Object.fromEntries(formData.entries());
 
-    // Send form data to my inbox.
-    const sent = await sendFormToMe(body, context.env.DKIM_PRIVATE_KEY);
+  const sent = await sendFormTo(body, context.env.EMAIL_BEN);
 
-    if (!sent) {
-        return new Response("Oops! Something went wrong. Please try submitting the form again.", { status: 500 });
-    }
+  if (!sent) {
+    return new Response("Oops! Something went wrong. Please try submitting the form again.", { status: 500 });
+  }
 
-    return new Response("Form submitted successfully!", { status: 200 });
+  return Response.redirect(context.request.headers.get("Referer"), 303);
 }
 
-// Send email to my inbox (email with form data)
-async function sendFormToMe(body, ENV) {
-    const send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
+async function sendFormTo(body, email) {
+  const send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+           to: [{ email }],
         },
-        body: JSON.stringify({
-            personalizations: [
-                {
-                    to: [{ email: ENV.EMAIL_BEN }],
-                },
-            ],
-            from: {
-                name: "Contact Form",
-                email: "no-reply@workermail.beh.uk", // CHANGE THIS to your email address (emails will be sent FROM this address).
-            },
-            subject: "New contact form submission",
-            content: [
-                {
-                    type: "text/plain",
-                    value: `Email: ${body.email}\nMessage: ${body.message}`,
-                },
-            ],
-        }),
-    });
+      ],
+      from: {
+        name: "Contact Form",
+        email: "no-reply@workermail.beh.uk",
+      },
+      subject: "New contact form submission",
+      content: [
+        {
+          type: "text/plain",
+          value: `Email: ${body.email}\nMessage: ${body.message}`,
+        },
+      ],
+    }),
+  });
 
-    // Get the response from the email API
-    const send_response = await fetch(send_request);
+  const send_response = await fetch(send_request);
 
-    // Check if the email was sent successfully.
-    if (!send_response.ok) {
-        return false;
-    }
+  if (!send_response.ok) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
